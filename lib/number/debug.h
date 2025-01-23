309bd94583f7e51e64a90ac2f7ddb34fad884816
Renan Souza <renan.rodrigues.souza1@gmail.com>
2025-01-23 19:00:24 -0300

    Add number (#2)
    * refactor
    
    * fix
    
    * finish test

--- lib/number/debug.h
+++ lib/number/debug.h
@@ -7,13 +7,18 @@
 
 #include <stdbool.h>
 
+number_p number_create_immed(uint64_t n, ...);
 bool number_immed(number_p num, uint64_t n, ...);
 
 #endif
 
-number_p number_create(uint64_t value);
+void number_display(number_p num);
+
+number_p number_create(uint64_t value, number_p next);
 void number_free(number_p num);
 
 number_p number_add_uint(number_p n, uint64_t value);
+
+number_p number_add(number_p num_1, number_p num_2);
 
 #endif
--- lib/number/test/test.c
+++ lib/number/test/test.c
@@ -8,13 +8,45 @@ void test_number_create()
 {
     printf("\n\t%s", __func__);
 
-    number_p num = number_create(1);
+    number_p num = number_create(1, NULL);
+    assert(num->value == 1);
+    assert(num->next  == NULL);
+    free(num);
+
+    num = number_create(2, (number_p)1);
+    assert(num->value == 2);
+    assert(num->next  == (number_p)1);
     free(num);
 
     assert(clu_mem_empty());
 }
 
-void test_number_add()
+void test_number_create_immed()
+{
+    printf("\n\t%s", __func__);
+
+    number_p num = number_create_immed(0);
+    assert(num == NULL);
+    number_free(num);
+
+    num = number_create_immed(1, 1);
+    assert(num->value == 1);
+    assert(num->next  == NULL);
+    number_free(num);
+
+    num = number_create_immed(2, 2, 1);
+    assert(num->value == 1);
+    assert(num->next  != NULL);
+    assert(num->next->value == 2);
+    assert(num->next->next  == NULL);
+    number_free(num);
+
+    assert(clu_mem_empty());
+}
+
+
+
+void test_number_add_uint()
 {
     printf("\n\t%s", __func__);
 
@@ -28,15 +60,65 @@ void test_number_add()
     assert(number_immed(num, 1, 1));
     number_free(num);
 
-    num = number_create(1);
+    num = number_create_immed(1, 1);
     num = number_add_uint(num, 2);
     assert(number_immed(num, 1, 3));
     number_free(num);
 
-    num = number_create(UINT64_MAX);
+    num = number_create_immed(1, UINT64_MAX);
     num = number_add_uint(num, 3);
-    assert(number_immed(num, 2, 2, 1));
+    assert(number_immed(num, 2, 1, 2));
     number_free(num);
+
+    assert(clu_mem_empty());
+}
+
+void test_number_add(bool show)
+{
+    printf("\n\t%s", __func__);
+
+    if(show) printf("\n\t\t%s 1", __func__);
+    number_p num_1, num_2;
+    num_1 = NULL;
+    num_2 = NULL;
+    num_1 = number_add(num_1, num_2);
+    assert(number_immed(num_1, 0));
+    number_free(num_1);
+    
+    if(show) printf("\n\t\t%s 2", __func__);
+    num_1 = NULL;
+    num_2 = number_create_immed(1, 1);
+    num_1 = number_add(num_1, num_2);
+    assert(number_immed(num_1, 1, 1));
+    number_free(num_1);
+    
+    if(show) printf("\n\t\t%s 3", __func__);
+    num_1 = number_create_immed(1, 1);
+    num_2 = NULL;
+    num_1 = number_add(num_1, num_2);
+    assert(number_immed(num_1, 1, 1));
+    number_free(num_1);
+    
+    if(show) printf("\n\t\t%s 4", __func__);
+    num_1 = number_create_immed(1, 1);
+    num_2 = number_create_immed(1, 2);
+    num_1 = number_add(num_1, num_2);
+    assert(number_immed(num_1, 1, 3));
+    number_free(num_1);
+    
+    if(show) printf("\n\t\t%s 5", __func__);
+    num_1 = number_create_immed(2, 2, 1);
+    num_2 = number_create_immed(1, 2);
+    num_1 = number_add(num_1, num_2);
+    assert(number_immed(num_1, 2, 2, 3));
+    number_free(num_1);
+    
+    if(show) printf("\n\t\t%s 6", __func__);
+    num_1 = number_create_immed(2, UINT64_MAX, UINT64_MAX);
+    num_2 = number_create_immed(1, 1);
+    num_1 = number_add(num_1, num_2);
+    assert(number_immed(num_1, 3, 1, 0, 0));
+    number_free(num_1);
 
     assert(clu_mem_empty());
 }
@@ -48,10 +130,13 @@ void test_number()
     printf("\n%s", __func__);
 
     test_number_create();
-    test_number_add();
+    test_number_create_immed();
+    test_number_add_uint();
+    test_number_add(false);
 
     assert(clu_mem_empty());
 }
+
 
 
 int main() 
