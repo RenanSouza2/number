2b4e90be82858b918164d450ad7064b2a97c5505
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-02-18 08:48:32 -0300

    retrieve mod lib

---
+++ lib/mod/debug.h
@@ -0,0 +1,23 @@
+#ifndef __MOD_DEBUG_H__
+#define __MOD_DEBUG_H__
+
+#include "struct.h"
+
+#ifdef DEBUG
+
+bool mod_immed(mod_t mod, uint64_t n, ...);
+
+#endif
+
+mod_t mod_create(num_p num, num_p p);
+mod_t mod_wrap(uint64_t value, num_p p);
+void mod_free(mod_t mod);
+
+void mod_display(mod_t mod);
+
+mod_t mod_add(mod_t mod_1, mod_t mod_2);
+mod_t mod_sub(mod_t mod_1, mod_t mod_2);
+mod_t mod_mul(mod_t mod_1, mod_t mod_2);
+mod_t mod_div(mod_t mod_1, mod_t mod_2);
+
+#endif
