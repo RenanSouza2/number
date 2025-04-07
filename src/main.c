21b240fe4787e17ce706a19eb3b3f5bfa983c145
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-04-07 20:44:59 -0300

    delete

--- src/main.c
+++ src/main.c
@@ -434,11 +434,11 @@ int main()
     srand(time(NULL));
 
     // num_generate(21, 2);
-    // time_1(14, 17);
+    time_1(14, 21);
     // time_2(argc, argv, 19);
     // fibonacci();
-    fibonacci_2(16, 23);
-    fibonacci_3(16, 23);
+    // fibonacci_2(16, 23);
+    // fibonacci_3(16, 23);
     // sqrt_2();
 
     // uint64_t pos = 1e1;
