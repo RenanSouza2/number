2e51cac25f230163304cea01a54a558bc93158d5
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-04-24 08:11:07 -0300

    test mod

--- lib/mod/test/test.c
+++ lib/mod/test/test.c
@@ -1,246 +1,314 @@
+#include "../debug.h"
 #include "../../num/debug.h"
 
-#include "../debug.h"
 #include "../../../mods/macros/test.h"
 #include "../../../mods/macros/assert.h"
 #include "../../../mods/clu/header.h"
 
 
 
+#define TEST_ASSERT_EMPTY assert(clu_mem_is_empty());
+
 void test_mod_create(bool show)
 {
-    char offset[] = "\t";
-    printf("\n%s%s\t\t", offset, __func__);
+    TEST_FN_OPEN
 
     num_t p = num_wrap(7);
 
-    if(show) printf("\n%s\t%s 1\t\t", offset, __func__);
-    num_t num = num_create_immed(0);
-    mod_t mod = mod_create(num, p);
-    assert(mod_immed(mod, 0));
+    TEST_CASE_OPEN(1)
+    {
+        num_t num = num_create_immed(0);
+        mod_t mod = mod_create(num, p);
+        assert(mod_immed(mod, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 2\t\t", offset, __func__);
-    num = num_create_immed(1, 1);
-    mod = mod_create(num, p);
-    assert(mod_immed(mod, 1, 1));
+    TEST_CASE_OPEN(2)
+    {
+        num_t num = num_create_immed(1, 1);
+        mod_t mod = mod_create(num, p);
+        assert(mod_immed(mod, 1, 1));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 3\t\t", offset, __func__);
-    num = num_create_immed(1, 7);
-    mod = mod_create(num, p);
-    assert(mod_immed(mod, 0));
+    TEST_CASE_OPEN(3)
+    {
+        num_t num = num_create_immed(1, 7);
+        mod_t mod = mod_create(num, p);
+        assert(mod_immed(mod, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 4\t\t", offset, __func__);
-    num = num_create_immed(1, 8);
-    mod = mod_create(num, p);
-    assert(mod_immed(mod, 1, 1));
+    TEST_CASE_OPEN(4)
+    {
+        num_t num = num_create_immed(1, 8);
+        mod_t mod = mod_create(num, p);
+        assert(mod_immed(mod, 1, 1));
+    }
+    TEST_CASE_CLOSE
 
     num_free(p);
-
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    
+    TEST_FN_CLOSE
 }
 
 void test_mod_wrap(bool show)
 {
-    char offset[] = "\t";
-    printf("\n%s%s", offset, __func__);
+    TEST_FN_OPEN
 
     num_t p = num_wrap(7);
 
-    if(show) printf("\n%s\t%s 1", offset, __func__);
-    mod_t mod = mod_wrap(0, p);
-    assert(mod_immed(mod, 0));
+    TEST_CASE_OPEN(1)
+    {
+        mod_t mod = mod_wrap(0, p);
+        assert(mod_immed(mod, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 2", offset, __func__);
-    mod = mod_wrap(2, p);
-    assert(mod_immed(mod, 1, 2));
+    TEST_CASE_OPEN(2)
+    {
+        mod_t mod = mod_wrap(2, p);
+        assert(mod_immed(mod, 1, 2));
+    }
+    TEST_CASE_CLOSE
 
     num_free(p);
 
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    TEST_FN_CLOSE
 }
 
 
 
 void test_mod_add(bool show)
 {
-    char offset[] = "\t";
-    printf("\n%s%s", offset, __func__);
+    TEST_FN_OPEN
 
     num_t p = num_wrap(7);
 
-    if(show) printf("\n%s\t%s 1", offset, __func__);
-    mod_t mod_1 = mod_wrap(0, p);
-    mod_t mod_2 = mod_wrap(0, p);
-    mod_t mod_res = mod_add(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
+    TEST_CASE_OPEN(1)
+    {
+        mod_t mod_1 = mod_wrap(0, p);
+        mod_t mod_2 = mod_wrap(0, p);
+        mod_t mod_res = mod_add(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 2", offset, __func__);
-    mod_1 = mod_wrap(1, p);
-    mod_2 = mod_wrap(2, p);
-    mod_res = mod_add(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 3));
+    TEST_CASE_OPEN(2)
+    {
+        mod_t mod_1 = mod_wrap(1, p);
+        mod_t mod_2 = mod_wrap(2, p);
+        mod_t mod_res = mod_add(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 3));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 3", offset, __func__);
-    mod_1 = mod_wrap(1, p);
-    mod_2 = mod_wrap(6, p);
-    mod_res = mod_add(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
+    TEST_CASE_OPEN(3)
+    {
+        mod_t mod_1 = mod_wrap(1, p);
+        mod_t mod_2 = mod_wrap(6, p);
+        mod_t mod_res = mod_add(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 4", offset, __func__);
-    mod_1 = mod_wrap(6, p);
-    mod_2 = mod_wrap(6, p);
-    mod_res = mod_add(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 5));
+    TEST_CASE_OPEN(4)
+    {
+        mod_t mod_1 = mod_wrap(6, p);
+        mod_t mod_2 = mod_wrap(6, p);
+        mod_t mod_res = mod_add(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 5));
+    }
+    TEST_CASE_CLOSE
 
     num_free(p);
 
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    TEST_FN_CLOSE
 }
 
 void test_mod_sub(bool show)
 {
-    char offset[] = "\t";
-    printf("\n%s%s", offset, __func__);
+    TEST_FN_OPEN
 
     num_t p = num_wrap(7);
 
-    if(show) printf("\n%s\t%s 1", offset, __func__);
-    mod_t mod_1 = mod_wrap(0, p);
-    mod_t mod_2 = mod_wrap(0, p);
-    mod_t mod_res = mod_sub(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
+    TEST_CASE_OPEN(1)
+    {
+        mod_t mod_1 = mod_wrap(0, p);
+        mod_t mod_2 = mod_wrap(0, p);
+        mod_t mod_res = mod_sub(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 2", offset, __func__);
-    mod_1 = mod_wrap(0, p);
-    mod_2 = mod_wrap(1, p);
-    mod_res = mod_sub(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 6));
+    TEST_CASE_OPEN(2)
+    {
+        mod_t mod_1 = mod_wrap(0, p);
+        mod_t mod_2 = mod_wrap(1, p);
+        mod_t mod_res = mod_sub(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 6));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 3", offset, __func__);
-    mod_1 = mod_wrap(2, p);
-    mod_2 = mod_wrap(0, p);
-    mod_res = mod_sub(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 2));
+    TEST_CASE_OPEN(3)
+    {
+        mod_t mod_1 = mod_wrap(2, p);
+        mod_t mod_2 = mod_wrap(0, p);
+        mod_t mod_res = mod_sub(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 2));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 4", offset, __func__);
-    mod_1 = mod_wrap(3, p);
-    mod_2 = mod_wrap(2, p);
-    mod_res = mod_sub(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 1));
+    TEST_CASE_OPEN(4)
+    {
+        mod_t mod_1 = mod_wrap(3, p);
+        mod_t mod_2 = mod_wrap(2, p);
+        mod_t mod_res = mod_sub(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 1));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 5", offset, __func__);
-    mod_1 = mod_wrap(2, p);
-    mod_2 = mod_wrap(5, p);
-    mod_res = mod_sub(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 4));
+    TEST_CASE_OPEN(5)
+    {
+        mod_t mod_1 = mod_wrap(2, p);
+        mod_t mod_2 = mod_wrap(5, p);
+        mod_t mod_res = mod_sub(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 4));
+    }
+    TEST_CASE_CLOSE
 
     num_free(p);
 
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    TEST_FN_CLOSE
 }
 
 void test_mod_mul(bool show)
 {
-    char offset[] = "\t";
-    printf("\n%s%s", offset, __func__);
+    TEST_FN_OPEN
 
     num_t p = num_wrap(7);
 
-    if(show) printf("\n%s\t%s 1", offset, __func__);
-    mod_t mod_1 = mod_wrap(0, p);
-    mod_t mod_2 = mod_wrap(0, p);
-    mod_t mod_res = mod_mul(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
+    TEST_CASE_OPEN(1)
+    {
+        mod_t mod_1 = mod_wrap(0, p);
+        mod_t mod_2 = mod_wrap(0, p);
+        mod_t mod_res = mod_mul(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 2", offset, __func__);
-    mod_1 = mod_wrap(0, p);
-    mod_2 = mod_wrap(1, p);
-    mod_res = mod_mul(mod_1, mod_2);
-    assert(mod_immed(mod_res,0));
+    TEST_CASE_OPEN(2)
+    {
+        mod_t mod_1 = mod_wrap(0, p);
+        mod_t mod_2 = mod_wrap(1, p);
+        mod_t mod_res = mod_mul(mod_1, mod_2);
+        assert(mod_immed(mod_res,0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 3", offset, __func__);
-    mod_1 = mod_wrap(1, p);
-    mod_2 = mod_wrap(0, p);
-    mod_res = mod_mul(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
+    TEST_CASE_OPEN(3)
+    {
+        mod_t mod_1 = mod_wrap(1, p);
+        mod_t mod_2 = mod_wrap(0, p);
+        mod_t mod_res = mod_mul(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 4", offset, __func__);
-    mod_1 = mod_wrap(2, p);
-    mod_2 = mod_wrap(3, p);
-    mod_res = mod_mul(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 6));
+    TEST_CASE_OPEN(4)
+    {
+        mod_t mod_1 = mod_wrap(2, p);
+        mod_t mod_2 = mod_wrap(3, p);
+        mod_t mod_res = mod_mul(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 6));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 4", offset, __func__);
-    mod_1 = mod_wrap(5, p);
-    mod_2 = mod_wrap(6, p);
-    mod_res = mod_mul(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 2));
+    TEST_CASE_OPEN(5)
+    {
+        mod_t mod_1 = mod_wrap(5, p);
+        mod_t mod_2 = mod_wrap(6, p);
+        mod_t mod_res = mod_mul(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 2));
+    }
+    TEST_CASE_CLOSE
+
+    TEST_CASE_OPEN(6)
+    {
+        num_t p_6 = num_wrap(6);
+
+        mod_t mod_1 = mod_wrap(3, p_6);
+        mod_t mod_2 = mod_wrap(2, p_6);
+        mod_t mod_res = mod_mul(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+
+        num_free(p_6);
+    }
+    TEST_CASE_CLOSE
 
     num_free(p);
-    p = num_wrap(6);
 
-    if(show) printf("\n%s\t%s 5", offset, __func__);
-    mod_1 = mod_wrap(3, p);
-    mod_2 = mod_wrap(2, p);
-    mod_res = mod_mul(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
-
-    num_free(p);
-
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    TEST_FN_CLOSE
 }
 
 void test_mod_div(bool show)
 {
-    char offset[] = "\t";
-    printf("\n%s%s", offset, __func__);
+    TEST_FN_OPEN
 
     num_t p = num_wrap(7);
 
-    if(show) printf("\n%s\t%s 1", offset, __func__);
-    mod_t mod_1 = mod_wrap(0, p);
-    mod_t mod_2 = mod_wrap(2, p);
-    mod_t mod_res = mod_div(mod_1, mod_2);
-    assert(mod_immed(mod_res, 0));
+    TEST_CASE_OPEN(1)
+    {
+        mod_t mod_1 = mod_wrap(0, p);
+        mod_t mod_2 = mod_wrap(2, p);
+        mod_t mod_res = mod_div(mod_1, mod_2);
+        assert(mod_immed(mod_res, 0));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 2", offset, __func__);
-    mod_1 = mod_wrap(6, p);
-    mod_2 = mod_wrap(2, p);
-    mod_res = mod_div(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 3));
+    TEST_CASE_OPEN(2)
+    {
+        mod_t mod_1 = mod_wrap(6, p);
+        mod_t mod_2 = mod_wrap(2, p);
+        mod_t mod_res = mod_div(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 3));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 3", offset, __func__);
-    mod_1 = mod_wrap(6, p);
-    mod_2 = mod_wrap(4, p);
-    mod_res = mod_div(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 5));
+    TEST_CASE_OPEN(3)
+    {
+        mod_t mod_1 = mod_wrap(6, p);
+        mod_t mod_2 = mod_wrap(4, p);
+        mod_t mod_res = mod_div(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 5));
+    }
+    TEST_CASE_CLOSE
 
-    if(show) printf("\n%s\t%s 4", offset, __func__);
+    TEST_CASE_OPEN(4)
+    {
+        num_t p_29 = num_wrap(29);
+
+        mod_t mod_1 = mod_wrap(1, p_29);
+        mod_t mod_2 = mod_wrap(9, p_29);
+        mod_t mod_res = mod_div(mod_1, mod_2);
+        assert(mod_immed(mod_res, 1, 13));
+
+        num_free(p_29);
+    }
+    TEST_CASE_CLOSE
+
     num_free(p);
-    p = num_wrap(29);
-    mod_1 = mod_wrap(1, p);
-    mod_2 = mod_wrap(9, p);
-    mod_res = mod_div(mod_1, mod_2);
-    assert(mod_immed(mod_res, 1, 13));
 
-    num_free(p);
-
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    TEST_FN_CLOSE
 }
 
 
 
 void test_mod()
 {
-    printf("\n%s", __func__);
+    TEST_LIB
 
-    bool show = false;
+    bool show = true;
 
     test_mod_create(show);
     test_mod_wrap(show);
@@ -250,8 +318,7 @@ void test_mod()
     test_mod_mul(show);
     test_mod_div(show);
 
-    chunk_pool_clean();
-    assert(clu_mem_is_empty());
+    TEST_ASSERT_EMPTY
 }
 
 
@@ -259,7 +326,7 @@ void test_mod()
 int main()
 {
     setbuf(stdout, NULL);
-    TEST_TIMEOUT_OPEN(5)
+    TEST_TIMEOUT_OPEN_DEFAULT
     test_mod();
     TEST_TIMEOUT_CLOSE
     printf("\n\n\tTest successful\n\n");
